f52a7a84c0f945a36d5540a66bf793efb9bebd6d
Jason Zaugg <jzaugg@gmail.com>
2018-04-09 11:48:54 +1000

    Declare jpms module names inline, handle absent modulename

--- build.sbt
+++ build.sbt
@@ -8,31 +8,28 @@ organization in ThisBuild := "de.sormuras"
 
 version in ThisBuild := "1.0-SNAPSHOT"
 
+autoScalaLibrary in ThisBuild := false
+
+crossPaths in ThisBuild := false
+
 val slf4jDep = "org.slf4j" % "slf4j-api" % System.getProperty("slf4j.version", "1.8.0-beta1")
 val apiguardianDep = "org.apiguardian" % "apiguardian-api" % System.getProperty("apiguardian.version", "1.0.0") % Test
 val jupiterVersion = System.getProperty("jupiter.version", "5.1.0")
-val junitJupiterApiDep = "org.junit.jupiter" % "junit-jupiter-api" % jupiterVersion % Test
+val junitJupiterApiDep = "org.junit.jupiter" % "junit-jupiter-api" % jupiterVersion % Test jpmsName "org.junit.jupiter.api"
 val junitJupiterEngineDep = "org.junit.jupiter" % "junit-jupiter-engine" % jupiterVersion % Test
 val junitJupiterConsoleDep = "org.junit.platform" % "junit-platform-console" % System.getProperty("platform.version", "1.1.0") % Test
 
-jpmsLibraryDependencyName in ThisBuild := {
-  List(junitJupiterApiDep -> "org.junit.jupiter.api")
-}
-
-autoScalaLibrary in ThisBuild := false
-crossPaths in ThisBuild := false
-
+// Non-forked runners in SBT don't support creation of a new ModuleLayer (https://docs.oracle.com/javase/9/docs/api/java/lang/ModuleLayer.html)
 fork in run in ThisBuild := true
-
 fork in test in ThisBuild := true
 
 val sawdustAlpha = project.in(file("modules/sawdust.alpha")).enablePlugins(JpmsPlugin).settings(
   jpmsModuleName := "sawdust.alpha",
-  jpmsPatchTest := true,
   libraryDependencies ++= Seq(slf4jDep, apiguardianDep, junitJupiterApiDep, junitJupiterEngineDep, junitJupiterConsoleDep)
 )
 
+// This project defines its own
 val userView = project.in(file("modules/user.view")).enablePlugins(JpmsPlugin).dependsOn(sawdustAlpha).settings(
-  jpmsModuleName := "user.view",
+  jpmsModuleName in Test := "user.view",
   libraryDependencies ++= Seq(slf4jDep, apiguardianDep, junitJupiterApiDep, junitJupiterEngineDep, junitJupiterConsoleDep)
 )
--- project/Jpms.scala
+++ project/Jpms.scala
@@ -4,9 +4,14 @@ import sbt.{AutoPlugin, Compile, Def, ModuleID, Setting, Test, inConfig, setting
 import sbt.Keys._
 
 object JpmsPlugin extends AutoPlugin{
+  private val JpmsNameKey = "jpmsName"
   object autoImport {
+    implicit class RichModuleId(private val self: ModuleID) {
+      def jpmsName(s: String) = self.withExtraAttributes(self.extraAttributes.updated(JpmsNameKey, s))
+    }
+
     val jpmsModuleName = settingKey[String]("JPMS module name")
-    val jpmsLibraryDependencyName = settingKey[List[(ModuleID, String)]]("JPMS module name of dependencies")
+    val jpmsLibraryDependencyName = settingKey[Seq[(ModuleID, String)]]("JPMS module name of dependencies")
     val jpmsPatchTest = settingKey[Boolean]("Test sources should be patched into the compile module")
   }
 
@@ -17,23 +22,45 @@ object JpmsPlugin extends AutoPlugin{
   override lazy val projectSettings: Seq[Setting[_]] = commonSettings
 
   def commonConfigSettings: Seq[Setting[_]] = Seq(
-    javaSource := (javaSource.value / jpmsModuleName.value),
+    javaSource := {
+      jpmsModuleName.?.value match {
+        case Some(modName) => (javaSource.value / modName)
+        case _ => javaSource.value
+      }
+    },
     javacOptions := overwriteModulePath(dependencyClasspath.value.map(_.data))(javacOptions.value),
     javaOptions := overwriteModulePath(dependencyClasspath.value.map(_.data) ++ (if (configuration.value != Test || !jpmsPatchTest.??(false).value) List(classDirectory.value) else Nil) )(javaOptions.value),
     // apparently this needs to follow `--patch-module`: https://gist.github.com/d696b44b817e6fd0b2e457ad805e62cb
     javaOptions := {
-      val modAndMainClass = jpmsModuleName.value + "/" + mainClass.value.getOrElse("")
-      overwriteOption("--module", modAndMainClass, moveToEnd = true)(javaOptions.value)
+      val main = mainClass.value.getOrElse("")
+      jpmsModuleName.?.value match {
+        case Some(modName) =>
+          val modAndMainClass = modName + "/" + main
+          overwriteOption("--module", modAndMainClass, moveToEnd = true)(javaOptions.value)
+        case None => javaOptions.value
+      }
     }
   )
 
+  private def patchModule(isJavaOptions: Boolean) = Def.task {
+    val modNameOpt = jpmsModuleName.?.value
+    val javaSourceValue = (javaSource in Test).value
+    val classDirectoryValue = (classDirectory in Test).value
+    val target = if (isJavaOptions) classDirectoryValue else javaSourceValue
+    if (jpmsPatchTest.??(false).value) {
+      modNameOpt match {
+        case Some(moduleName) =>
+          List(
+            "--patch-module", moduleName + "=" + target.toString
+          )
+        case _ => Nil
+      }
+    } else Nil
+  }
+
   def patchModuleSettings: Seq[Setting[_]] = Seq(
-    javacOptions in Test ++= (if (jpmsPatchTest.??(false).value) List(
-      "--patch-module", jpmsModuleName.value + "=" + (javaSource in Test).value
-    ) else Nil),
-    javaOptions in Test ++= (if (jpmsPatchTest.??(false).value) List(
-      "--patch-module", jpmsModuleName.value + "=" + (classDirectory in Test).value
-    ) else Nil)
+    javacOptions in Test ++= patchModule(isJavaOptions = false).value,
+    javaOptions in Test ++= patchModule(isJavaOptions = true).value
   )
 
   private def testAddModulesAddReads(isJavaOptions: Boolean): Def.Initialize[Task[Seq[String]]] = Def.task  {
@@ -41,21 +68,21 @@ object JpmsPlugin extends AutoPlugin{
     val compileFileSet = compileDeps.iterator.map(_.data)
     val testDeps = (dependencyClasspath in Test).value
     val testOnlyDeps = testDeps.filterNot(dep => compileFileSet.contains(dep.data))
-    println(testOnlyDeps)
     val map: Map[(String, String), String] = jpmsLibraryDependencyName.value.iterator.map(it => ((it._1.organization, it._1.name), it._2)).toMap
     val testOnlyJpmsNames: Seq[String] = testOnlyDeps.flatMap(dep => dep.get(moduleID.key).flatMap((m: ModuleID) => map.get((m.organization, m.name))))
-    println(jpmsLibraryDependencyName.value)
-    println("testOnlyJpmsNames = " + testOnlyJpmsNames)
+    val testModuleName = (jpmsModuleName in Test).?.value
+    val compileModuleName = (jpmsModuleName in Compile).?.value
     if (testOnlyJpmsNames.isEmpty) Nil
     else {
       val jpmsModulePath = testOnlyJpmsNames.mkString(java.io.File.pathSeparator)
-      val sameModuleNameAsCompile = (jpmsModuleName in Test).value == (jpmsModuleName in Compile).value
       val addModules = List("--add-modules", if (isJavaOptions) "ALL-MODULE-PATH" else testOnlyJpmsNames.mkString(","))
-      if (!sameModuleNameAsCompile) addModules
-      else {
-        val addReads = testOnlyJpmsNames.distinct.flatMap(name => List("--add-reads", jpmsModuleName.value + "=" + name))
-        addModules ++ addReads
+      val addReads = (testModuleName, compileModuleName) match {
+        case (Some(t), Some(c)) if t == c =>
+          testOnlyJpmsNames.distinct.flatMap(name => List("--add-reads", c + "=" + name))
+        case _ =>
+          Nil
       }
+      addModules ++ addReads
     }
   }
 
@@ -64,7 +91,21 @@ object JpmsPlugin extends AutoPlugin{
     javaOptions in Test ++= testAddModulesAddReads(isJavaOptions = true).value
   ) ++ patchModuleSettings
 
-  def commonSettings: Seq[Setting[_]] = (
+  def commonSettings: Seq[Setting[_]] = Seq(
+    jpmsLibraryDependencyName := {
+      val deps = libraryDependencies.value
+      val extra = libraryDependencies.value.map(dep => (dep, dep.extraAttributes.get(JpmsNameKey)))
+      extra.collect { case (x, Some(y)) => (x, y)}
+    },
+    jpmsPatchTest := {
+      val testModuleName = (jpmsModuleName in Test).?.value
+      val compileModuleName = (jpmsModuleName in Compile).?.value
+      (testModuleName, compileModuleName) match {
+        case (Some(t), Some(c)) if t == c => true
+        case _ => false
+      }
+    }
+  ) ++ (
     testModuleSettings ++ Seq(Compile, Test).flatMap(scope => inConfig(scope)(commonConfigSettings))
   )
 
